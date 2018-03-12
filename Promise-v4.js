71d268d0f439a818cc12e02f4aea9910d1687ceb
mickey0524 <buptbh@163.com>
2018-03-12 19:35:09 +0800

    feat(promise): 增加Promise-v4 支持串行链式调用

---
+++ Promise-v4.js
@@ -0,0 +1,51 @@
+/**
+ * then 方法返回一个 new 出来的Promise用来作为桥接Promise
+ * 这样实现了串形链式调用
+ * 关键就是给then里面return 的 promise 注册一个 桥接 promise 的 resolve 函数
+ */
+function Promise(fn) {
+  var state = 'pending',
+    value = null,
+    callbacks = [];
+
+  this.then = function(onFulfilled) {
+    return new Promise(resolve => {
+      handle({
+        onFulfilled: onFulfilled,
+        resolve: resolve,
+      });
+    });
+  }
+
+  function handle(callback) {
+    if (state === 'pending') {
+      callback.push(callback);
+      return;
+    }
+    if (!callback.onFulfilled) {
+      callback.resolve(value);
+      return;
+    }
+    var ret = callback.onFulfilled(value);
+    callback.resolve(ret);
+  }
+
+  function resolve(newValue) {
+    if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+      var then = newValue.then;
+      if (typeof then === 'function') {
+        then.call(newValue, resolve);
+        return;
+      }
+    }
+    state = 'resolved';
+    value = newValue;
+    setTimeout(() => {
+      callbacks.forEach(callback => {
+        handle(callback);
+      });
+    }, 0);
+  }
+
+  fn(resolve);
+}
